f8fd4397580c2946872edabb2de2c58ab207fd04
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2025-08-20 15:01:03 -0500

    urlencode db username and pwd

--- secrets.tf
+++ secrets.tf
@@ -67,7 +67,7 @@ resource "google_secret_manager_secret" "db_connection" {
 resource "google_secret_manager_secret_version" "db_connection" {
   count       = var.enable_database ? 1 : 0
   secret      = google_secret_manager_secret.db_connection[0].id
-  secret_data = "postgresql+psycopg://${local.database_user}:${local.url_encoded_db_pwd}@${google_sql_database_instance.main[0].private_ip_address}:5432/${local.database_name}"
+  secret_data = "postgresql+psycopg://${urlencode(local.database_user)}:${urlencode(random_password.db_password[0].result)}@${google_sql_database_instance.main[0].private_ip_address}:5432/${local.database_name}"
 }
 
 # Secret Manager secrets for config files
